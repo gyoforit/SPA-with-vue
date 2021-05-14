419b01561552b557c14aba76813435d2c1c76c16
ryu <munam4@naver.com>
2021-05-14 19:21:26 +0900

    pjt10 수정

--- src/views/Random.vue
+++ src/views/Random.vue
@@ -2,10 +2,34 @@
   <div>
     <h1>랜덤 영화 뽑기 🎲</h1>
     <button class="btn btn-sm btn-dark" @click="getRandomMovie">Pick!</button>
-    <p v-if="selectMovie">
+    <div class="my-3"></div>
+    <h2 v-if="selectMovie">
       오늘의 영화는!
-      {{ selectMovie[0].title }}
-    </p>
+    </h2>
+    <!-- <div v-if="selectMovie" class="card m-0" style="width: 30rem;">
+      <img :src="'http://image.tmdb.org/t/p/w500/' + selectMovie[0].poster_path " class="card-img-top" alt="">
+      <div class="card-body">
+        <h3 class="card-title">{{ selectMovie[0].title }}</h3>
+        <p class="card-text">{{ selectMovie[0].overview }}</p>
+      </div>
+    </div> -->
+    <div class="d-flex justify-content-center mb-5">
+      <div v-if="selectMovie" class="card text-center mt-3 w-25">
+        <div class="card-header">
+          <h3 class="card-title m-0">{{ selectMovie[0].title }}</h3>
+        </div>
+        <div class="card-body">
+          <img :src="'http://image.tmdb.org/t/p/w500/' + selectMovie[0].poster_path " class="card-img-top rounded mx-auto d-block" alt="">
+          <p class="card-text">{{ selectMovie[0].overview }}</p>
+        </div>
+        <div class="card-footer text-muted">
+          평점 : {{ selectMovie[0].vote_average }}
+          <br>
+          개봉일 : {{ selectMovie[0].release_date }}
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
