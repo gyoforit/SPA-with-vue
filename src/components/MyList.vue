56be1cbe38aec2f69bbdf20ad01044e0d6c3ca68
gyoforit <gyoforit@gmail.com>
2021-05-15 19:27:40 +0900

    Update README (modal error)

--- src/components/MyList.vue
+++ src/components/MyList.vue
@@ -5,28 +5,27 @@
         <p :class="{ watched: myMovie.watched }" class="d-inline" @click="updateMyMovie(myMovie)">
           {{ myMovie.title }}
         </p>
-        <button type="button" class="d-inline btn btn-sm btn-dark" data-bs-toggle="modal" data-bs-target="#deleteModal">x</button>
-        <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="deleteModalLabel">알림</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-body">
-                정말로 삭제하시겠습니까?
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">아니오</button>
-                <button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="deleteMyMovie(myMovie.idx)">네</button>
-              </div>
-            </div>
+      </div>
+      <button type="button" class="d-inline btn btn-sm btn-dark" data-bs-toggle="modal" data-bs-target="#deleteModal">x</button>
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="deleteModalLabel">알림</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            정말로 삭제하시겠습니까?
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">아니오</button>
+            <button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="deleteMyMovie(myMovie)">네</button>
           </div>
         </div>
       </div>
-
     </div>
-    <!-- Modal -->
   </div>
 </template>
 
